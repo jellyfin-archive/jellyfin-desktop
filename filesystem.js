774fb8426659d94104e1aa5ee84a3e8b4e090d3f
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-02-03 14:59:06 -0500

    rework file system

--- filesystem.js
+++ filesystem.js
@@ -1,37 +1,28 @@
 ﻿define([], function () {
 
+    function exits(endpoint, path) {
+        return new Promise(function (resolve, reject) {
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'electronfs://' + endpoint + '?path=' + path, true);
+            xhr.onload = function () {
+                if (this.response == 'true') {
+                    resolve();
+                } else {
+                    reject();
+                }
+            };
+            xhr.onerror = reject;
+            xhr.send();
+        });
+    }
+
     return {
         fileExists: function (path) {
-            return new Promise(function (resolve, reject) {
-
-                var xhr = new XMLHttpRequest();
-                xhr.open('POST', 'electronfs://fileexists?path=' + path, true);
-                xhr.onload = function () {
-                    if (this.response == 'true') {
-                        resolve();
-                    } else {
-                        reject();
-                    }
-                };
-                xhr.onerror = reject;
-                xhr.send();
-            });
+            return exits('fileexists', path);
         },
         directoryExists: function (path) {
-            return new Promise(function (resolve, reject) {
-
-                var xhr = new XMLHttpRequest();
-                xhr.open('POST', 'electronfs://directoryexists?path=' + path, true);
-                xhr.onload = function () {
-                    if (this.response == 'true') {
-                        resolve();
-                    } else {
-                        reject();
-                    }
-                };
-                xhr.onerror = reject;
-                xhr.send();
-            });
+            return exits('directoryexists', path);
         }
     };
 });
