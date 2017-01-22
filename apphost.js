506ae94db581734f41f8c85bd4557812549d3af7
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-01-21 23:46:58 -0500

    2.5.38

--- apphost.js
+++ apphost.js
@@ -41,44 +41,57 @@
                window.msSpeechRecognition;
     }
 
+    var supportedFeatures = function () {
+
+        var features = [
+            'windowstate',
+            'exit',
+            'runatstartup',
+            'filedownload',
+            'sharing',
+            'externallinks',
+            'sleep',
+            //'restart',
+            'shutdown'
+        ];
+
+        if (appStartInfo.supportsTransparentWindow) {
+            features.push('windowtransparency');
+        }
+
+        if (supportsVoiceInput()) {
+            features.push('voiceinput');
+        }
+
+        if (self.ServiceWorkerSyncRegistered) {
+            features.push('sync');
+        }
+
+        features.push('connectsignup');
+
+        features.push('soundeffects');
+        features.push('displaymode');
+        features.push('plugins');
+        features.push('exitmenu');
+        features.push('htmlaudioautoplay');
+        features.push('htmlvideoautoplay');
+        features.push('fullscreenchange');
+        features.push('displayableversion');
+
+        //features.push('remotecontrol');
+
+        features.push('multiserver');
+        features.push('imageanalysis');
+
+        return features;
+    }();
+
     return {
         getWindowState: getWindowState,
         setWindowState: setWindowState,
         supports: function (command) {
 
-            var features = [
-                'windowstate',
-                'exit',
-                'runatstartup',
-                'filedownload',
-                'sharing',
-                'externallinks',
-                'sleep',
-                //'restart',
-                'shutdown'
-            ];
-
-            if (appStartInfo.supportsTransparentWindow) {
-                features.push('windowtransparency');
-            }
-
-            if (supportsVoiceInput()) {
-                features.push('voiceinput');
-            }
-
-            features.push('soundeffects');
-            features.push('displaymode');
-            features.push('plugins');
-            features.push('exitmenu');
-            features.push('htmlaudioautoplay');
-            features.push('htmlvideoautoplay');
-            features.push('fullscreenchange');
-            features.push('displayableversion');
-
-            features.push('multiserver');
-            features.push('imageanalysis');
-
-            return features.indexOf(command.toLowerCase()) != -1;
+            return supportedFeatures.indexOf(command.toLowerCase()) !== -1;
         },
         capabilities: function () {
             return {
