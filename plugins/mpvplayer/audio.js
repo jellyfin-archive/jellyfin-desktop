c040ac14a3951711b101f4b6cce08c0ed9b60a3c
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-04-15 15:19:29 -0400

    stub out settings screens

---
+++ plugins/mpvplayer/audio.js
@@ -0,0 +1,58 @@
+﻿define(['loading', 'scrollHelper', 'appSettings', 'emby-select', 'emby-checkbox'], function (loading, scrollHelper, appSettings) {
+
+    return function (view, params) {
+
+        var self = this;
+
+        view.addEventListener('viewbeforeshow', function (e) {
+
+            var isRestored = e.detail.isRestored;
+
+            Emby.Page.setTitle('Audio Settings');
+
+            loading.hide();
+
+            if (!isRestored) {
+                scrollHelper.centerFocus.on(view.querySelector('.smoothScrollY'), false);
+                renderSettings();
+            }
+        });
+
+        view.addEventListener('viewbeforehide', saveSettings);
+
+        function saveSettings() {
+
+            var selectAudioBitstreamingMode = view.querySelector('.selectAudioBitstreamingMode');
+            config.AudioConfig.AudioBitstreaming = selectAudioBitstreamingMode.value;
+
+            var selectAudioRenderer = view.querySelector('.selectAudioRenderer');
+            config.AudioConfig.Renderer = selectAudioRenderer.value;
+
+            var selectSpeakerLayout = view.querySelector('.selectSpeakerLayout');
+            config.AudioConfig.SpeakerLayout = selectSpeakerLayout.value;
+
+            var selectDrc = view.querySelector('.selectDrc');
+            config.AudioConfig.EnableDRC = selectDrc.value != '';
+            config.AudioConfig.DRCLevel = selectDrc.value || '100';
+
+            config.AudioConfig.ExpandMono = view.querySelector('.selectExpandMono').value == 'true';
+            config.AudioConfig.Expand61 = view.querySelector('.selectExpandSixToSeven').value == 'true';
+        }
+
+        function renderSettings() {
+            var selectAudioBitstreamingMode = view.querySelector('.selectAudioBitstreamingMode');
+            selectAudioBitstreamingMode.value = config.AudioConfig.AudioBitstreaming;
+
+            var selectAudioRenderer = view.querySelector('.selectAudioRenderer');
+            selectAudioRenderer.value = config.AudioConfig.Renderer;
+
+            view.querySelector('.selectSpeakerLayout').value = config.AudioConfig.SpeakerLayout;
+
+            view.querySelector('.selectDrc').value = config.AudioConfig.EnableDRC ? config.AudioConfig.DRCLevel : '';
+
+            view.querySelector('.selectExpandMono').value = config.AudioConfig.ExpandMono;
+            view.querySelector('.selectExpandSixToSeven').value = config.AudioConfig.Expand61;
+        }
+    }
+
+});
---
+++ plugins/mpvplayer/video.js
@@ -0,0 +1,49 @@
+﻿define(['loading', 'pluginManager', 'scrollHelper', 'appSettings', 'emby-select', 'emby-checkbox'], function (loading, pluginManager, scrollHelper, appSettings) {
+
+    return function (view, params) {
+
+        var self = this;
+
+        view.addEventListener('viewbeforeshow', function (e) {
+
+            var isRestored = e.detail.isRestored;
+
+            Emby.Page.setTitle('Video Settings');
+
+            loading.hide();
+
+            if (!isRestored) {
+                scrollHelper.centerFocus.on(view.querySelector('.smoothScrollY'), false);
+
+                renderSettings();
+            }
+        });
+
+        view.addEventListener('viewbeforehide', saveSettings);
+
+        function saveSettings() {
+
+            var selectHwaMode = view.querySelector('.selectHwaMode');
+            config.VideoConfig.HwaMode = selectHwaMode.value;
+
+            var selectRefreshRateMode = view.querySelector('.selectRefreshRateMode');
+            config.VideoConfig.AutoChangeRefreshRate = selectRefreshRateMode.value;
+
+            var selectNominalRange = view.querySelector('.selectNominalRange');
+            config.VideoConfig.NominalRange = selectNominalRange.value;
+        }
+
+        function renderSettings() {
+
+            var selectHwaMode = view.querySelector('.selectHwaMode');
+            selectHwaMode.value = config.VideoConfig.HwaMode;
+
+            var selectRefreshRateMode = view.querySelector('.selectRefreshRateMode');
+            selectRefreshRateMode.value = config.VideoConfig.AutoChangeRefreshRate;
+
+            var selectNominalRange = view.querySelector('.selectNominalRange');
+            selectNominalRange.value = config.VideoConfig.NominalRange;
+        }
+    }
+
+});
