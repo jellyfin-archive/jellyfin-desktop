dee72498f29ecd1c47a59bad93ee075a2ca24fcc
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-09-17 02:17:23 -0400

    add onAppClose

--- scripts/appclose.js
+++ scripts/appclose.js
@@ -7,9 +7,7 @@ require(['playbackManager'], function (playbackManager) {
             // Prevent backwards navigation from stopping video
             history.back = function () { };
 
-            if (playbackManager.isPlaying()) {
-                playbackManager.stop();
-            }
+            playbackManager.onAppClose();
         }
     };
 });
