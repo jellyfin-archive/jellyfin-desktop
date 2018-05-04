0af1de943ec28a0b011c0f8e847989592d02a1d2
Luke Pulverenti <luke.pulverenti@gmail.com>
2018-05-04 11:10:45 -0400

    move wakeonlan

--- main.js
+++ main.js
@@ -445,7 +445,7 @@
                         paths: {
                             apphost: customFileProtocol + '://apphost',
                             shell: customFileProtocol + '://shell',
-                            wakeonlan: customFileProtocol + '://wakeonlan',
+                            wakeonlan: customFileProtocol + '://wakeonlan/wakeonlan',
                             serverdiscovery: customFileProtocol + '://serverdiscovery/serverdiscovery',
                             fullscreenmanager: 'file://' + replaceAll(path.normalize(topDirectory + '/fullscreenmanager.js'), '\\', '/'),
                             filesystem: customFileProtocol + '://filesystem'
