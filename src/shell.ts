83f1a02be4a4639717ee3a215a664dab248a46fc
Cromefire_ <cromefire_@outlook.com>
2020-05-03 17:31:44 +0200

    Formatted file

--- src/shell.ts
+++ src/shell.ts
@@ -1,11 +1,8 @@
-﻿define(['events', 'apphost'], function (events, apphost) {
-
+﻿define(["events", "apphost"], function (events, apphost) {
     function sendCommand(name) {
-
         return new Promise(function (resolve, reject) {
-
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', 'electronapphost://' + name, true);
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", `electronapphost://${name}`, true);
 
             xhr.onload = function () {
                 if (this.response) {
@@ -19,18 +16,14 @@
         });
     }
 
-    var shell = {};
-    var closingWindowState;
+    const shell = {};
+    let closingWindowState;
 
     function getProcessClosePromise(pid) {
-
         // returns a promise that resolves or rejects when a process closes
         return new Promise(function (resolve, reject) {
-
-            events.on(shell, 'closed', function (e, processId, error) {
-
+            events.on(shell, "closed", function (e, processId, error) {
                 if (processId === pid) {
-
                     if (closingWindowState) {
                         apphost.setWindowState(closingWindowState);
                         closingWindowState = null;
@@ -47,55 +40,48 @@
     }
 
     window.onChildProcessClosed = function (processId, error) {
-
-        events.trigger(shell, 'closed', [processId, error]);
+        events.trigger(shell, "closed", [processId, error]);
     };
 
     function paramsToString(params) {
+        const values = [];
 
-        var values = [];
+        for (const key in params) {
+            const value = params[key];
 
-        for (var key in params) {
-
-            var value = params[key];
-
-            if (value !== null && value !== undefined && value !== '') {
-                values.push(encodeURIComponent(key) + "=" + encodeURIComponent(value));
+            if (value !== null && value !== undefined && value !== "") {
+                values.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`);
             }
         }
-        return values.join('&');
+        return values.join("&");
     }
 
     shell.openUrl = function (url) {
-        return sendCommand('openurl?url=' + url);
+        return sendCommand(`openurl?url=${url}`);
     };
 
     shell.canExec = true;
 
     shell.close = function (processId) {
-
-        var url = 'shellclose?id=' + processId;
+        const url = `shellclose?id=${processId}`;
 
         return sendCommand(url);
     };
 
     shell.exec = function (options) {
-
-        var url = 'shellstart?' + paramsToString(options);
+        const url = `shellstart?${paramsToString(options)}`;
 
         return sendCommand(url).then(function (response) {
-
-            if (apphost.supports('windowstate')) {
-
+            if (apphost.supports("windowstate")) {
                 closingWindowState = apphost.getWindowState();
-                apphost.setWindowState('Minimized');
+                apphost.setWindowState("Minimized");
             }
 
-            events.trigger(shell, 'exec', [response]);
+            events.trigger(shell, "exec", [response]);
 
             return {
                 id: response,
-                promise: getProcessClosePromise(response)
+                promise: getProcessClosePromise(response),
             };
         });
     };
